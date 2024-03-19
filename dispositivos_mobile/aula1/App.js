959d495b3920c3fadab05906cf7f163b93557512
lisperey <pilrss16@gmail.com>
2024-03-18 22:57:40 -0300

    Finalizando atividade 2

--- dispositivos_mobile/aula1/App.js
+++ dispositivos_mobile/aula1/App.js
@@ -1,80 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState, createContext, useContext } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-const ThemeContext = createContext();
+import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Atividade1 from './componentes/atividade1';
+import FlexboxExample from './componentes/componente';
 
-const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
-
-  const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  };
-
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
-};
-
-const ThemeComponent = () => {
-  const { theme, toggleTheme } = useContext(ThemeContext);
-
-  return (
-    <div>
-      <Text>Tema: {theme}</Text>
-      <Button onPress={toggleTheme} title='Alterar tema'/>
-    </div>
-  );
-};
-
-const Counter = () =>{
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <Text>Valor: {count}</Text>
-      <Button onPress={()=>setCount(count+1)} title='Incrementar'/>
-    </div>
-  );
-}
-
-const ButtonCallback = () =>{
-  return (
-    <div>
-      <Button onPress={() => console.log('Botão clicado!')} title='Press'></Button>
-    </div>
-  )
-}
-
-const StateController = () =>{
-  const [inputValue, setValue] = useState('');
-  const changeText = (e)=>{
-    setValue(e.target.value);
-  }
-  return(
-    <div>
-      <p>{inputValue}</p>
-      <br/>
-      <input value={inputValue} onChange={changeText} placeholder="escreva..."></input>
-    </div>
-  )
-}
+const Stack = createStackNavigator();
 
 
 export default function App() {
   
   return (
-
-    <View style={styles.container}>
-      <ThemeProvider>
-        <Counter/>
-        <ThemeComponent/>
-        <ButtonCallback/>
-        <StateController/>
-      </ThemeProvider>
-
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Screen1" component={Atividade1} />
+          <Stack.Screen name="Screen2" component={FlexboxExample} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
